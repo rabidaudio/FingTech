6760ac9a2c2d0b6cd4b4d86def31ffc196dbb106
Charles Julian Knight <charles@rabidaudio.com>
2015-11-07 13:19:58 -0500

    initial commit

---
+++ FingTech/CreditCard.swift
@@ -0,0 +1,21 @@
+//
+//  CreditCard.swift
+//  FingTech
+//
+//  Created by fixd on 11/7/15.
+//  Copyright © 2015 fixd. All rights reserved.
+//
+
+import Foundation
+
+class CreditCard {
+    
+    var holderName:String?
+    
+    var cardNumber:Int?
+    
+    var ccv:Int?
+    
+    var zipcode:Int?
+
+}
--- FingTech/FirstViewController.swift
+++ FingTech/FirstViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet
+    var tableView: UITableView!
+    
+    var items: [String] = ["We", "Heart", "Swift"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        print(tableView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,24 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("asked for count (\(items.count))")
+        return items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
+        
+        cell.textLabel?.text = items[indexPath.row]
+        cell.detailTextLabel?.text = "ohai"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell \(indexPath.row)!")
+    }
 
 }
 
